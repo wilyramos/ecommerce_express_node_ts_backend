5dc2790d07bc10c677c4480946054e983550850f
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-07-05 13:00:37 -0500

    update validacion middleware

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -4,7 +4,14 @@ import { validationResult } from 'express-validator';
 export const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
     let errors = validationResult(req);
     if(!errors.isEmpty()){
+
+        /* Para retornar todos los errores 
         res.status(400).json({errors: errors.array()});
+        return;
+
+        */
+        const firstMessage = errors.array()[0].msg || "Error de validación";
+        res.status(400).json({ message: firstMessage });
         return;
     }
     next();
